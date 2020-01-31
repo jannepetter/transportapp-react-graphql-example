5a54b50d66dde8a80425488dbccdd5df4dd47164
Janne <peterdev80@gmail.com>
2020-01-31 17:57:56 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import gql from 'graphql-tag'
+import { useQuery, useMutation, useSubscription, useApolloClient } from "@apollo/react-hooks"
 
-const App = () => {
+const AGENCIES = gql`	
+{
+  agencies {
+    id
+    name
+  }
+}
+`
+
+const App: React.FC = () => {
+  const agencies = useQuery(AGENCIES)
+  console.log(agencies, 'check if workss')
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      hello
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,57 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'		
+import ReactDOM from 'react-dom'
+import App from './App'  
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { ApolloProvider } from '@apollo/react-hooks'	//baseconfig
+import { ApolloClient } from 'apollo-client'          //baseconfig
+import { InMemoryCache } from 'apollo-cache-inmemory' //baseconfig
+import { setContext } from 'apollo-link-context'      //baseconfig
+import { createHttpLink } from "apollo-link-http";
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import { split } from 'apollo-link'                   //subscription
+import { WebSocketLink } from 'apollo-link-ws'        //subscription
+import { getMainDefinition } from 'apollo-utilities'  //subscription
+
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,   //pass correct ws uri and reconnect true
+  options: { reconnect: false }
+})
+
+const httplink=createHttpLink({ 
+    uri:'https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql',
+  credentials:''                        //'include'   if cookies  
+}) 
+
+const authLink = setContext((_, { headers }) => {
+  const token = 'thisisyourtoken'
+ 
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer ${token}` : null,
+    }
+  }
+})
+
+const link = split(
+    ({ query }) => {
+        const definition = getMainDefinition(query);
+        return definition.kind === 'OperationDefinition' && definition.operation === 'subscription';
+      },
+  wsLink,
+  authLink.concat(httplink),
+)
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById('root')) 
+
+ 
+ 
