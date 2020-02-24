f260af4ebc79b6711e9457aec1a422365d84895c
Janne <peterdev80@gmail.com>
2020-02-24 12:36:43 +0200

    Updates

---
+++ precache-manifest.49d89566e9bc2196f65ccc875a7b08d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbfcb78c71224cfed45cbf8a53934e08",
+    "url": "/transportapp-react-graphql-example/index.html"
+  },
+  {
+    "revision": "39ffb276b347a387cc1d",
+    "url": "/transportapp-react-graphql-example/static/css/main.892eeb28.chunk.css"
+  },
+  {
+    "revision": "1365f8695ea8a1554bc1",
+    "url": "/transportapp-react-graphql-example/static/js/2.a9b05645.chunk.js"
+  },
+  {
+    "revision": "de1cedeacf4641d6277b42334cfacb1d",
+    "url": "/transportapp-react-graphql-example/static/js/2.a9b05645.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "39ffb276b347a387cc1d",
+    "url": "/transportapp-react-graphql-example/static/js/main.5e4e5294.chunk.js"
+  },
+  {
+    "revision": "d5debc6280212ae3cbde",
+    "url": "/transportapp-react-graphql-example/static/js/runtime-main.937fdd4c.js"
+  }
+]);
